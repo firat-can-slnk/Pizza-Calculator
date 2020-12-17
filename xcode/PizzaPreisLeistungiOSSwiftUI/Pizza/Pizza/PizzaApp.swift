00f6f79c3878cf9553e2a7b9aa49a7ff89ca471f
waffelcop <waffelcop@e205e917-7f67-47fe-9039-536ac8ca0758>
2020-12-17 13:08:19 +0000

    Pizza Währungsumrechnung, 2.0.5 Release

--- xcode/PizzaPreisLeistungiOSSwiftUI/Pizza/Pizza/PizzaApp.swift
+++ xcode/PizzaPreisLeistungiOSSwiftUI/Pizza/Pizza/PizzaApp.swift
@@ -11,7 +11,22 @@ import SwiftUI
 struct PizzaApp: App {
     let persistenceController = PersistenceController.shared
     
-    
+    init() {
+        let currencyConverter = CurrencyConverter()
+        
+        currencyConverter.updateExchangeRates(completion: {
+                    // The code inside here runs after all the data is fetched.
+                    
+                    // Now you can convert any currency:
+                    // • Example_1 (USD to EUR):
+            let doubleResult = currencyConverter.convert(10, valueCurrencyConverterCurrency: CurrencyConverterCurrency.USD, outputCurrencyConverterCurrency: CurrencyConverterCurrency.EUR)
+                    print("••• 10 USD = \(doubleResult!) EUR •••")
+                    
+                    // • Example_2 (EUR to GBP) - Returning a formatted String:
+            let formattedResult = currencyConverter.convertAndFormat(10, valueCurrencyConverterCurrency: CurrencyConverterCurrency.EUR, outputCurrencyConverterCurrency: CurrencyConverterCurrency.GBP, numberStyle: .decimal, decimalPlaces: 4)
+                    print("••• Formatted Result (10 EUR to GBP): \(formattedResult!) •••")
+                })
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
